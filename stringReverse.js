9c3aa5e8e6ebac7648c5382eb44284c79efc8625
Suvro Biswas <shuvo.249963@gmail.com>
2026-01-24 17:56:02 +0600

    Solve the problem using method

--- stringReverse.js
+++ stringReverse.js
@@ -1,9 +1,19 @@
-function reverseString(str) {
-    let reversed = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversed = reversed + str[i];
-    }
+// function reverseString(str) {
+//     let reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed = reversed + str[i];
+//     }
+//     return reversed;
+// }
+
+// console.log("After reversing the string the output is : ", reverseString('hello'))
+
+
+
+function reversedString(str){
+    const reversed = str.split('').reverse().join('')
     return reversed;
 }
 
-console.log("After reversing the string the output is : ", reverseString('hello'))
+const result = reversedString('hello')
+console.log(result)
