3ae6a2cb4cec35df5508edeaae948276661c280b
Suvro Biswas <shuvo.249963@gmail.com>
2026-01-24 18:01:26 +0600

    Find max number problem solve with method

--- findMaxNumber.js
+++ findMaxNumber.js
@@ -1,13 +1,22 @@
-function findMax(arr) {
-    let max = arr[0];
-    for (let num of arr) {
-        if (num > max) 
-        {
-             max = num;
-        }
+// function findMax(arr) {
+//     let max = arr[0];
+//     for (let num of arr) {
+//         if (num > max) 
+//         {
+//              max = num;
+//         }
            
-    }
-    return max;
+//     }
+//     return max;
+// }
+
+// console.log('The max number among the array is', findMax([5, 1, 9, 3]))
+
+
+function findMaxNum(arr){
+    const result = Math.max(...arr)
+    return result;
 }
 
-console.log('The max number among the array is', findMax([5, 1, 9, 3]))
+const number = findMaxNum([1,3,4,6,7])
+console.log(number)
