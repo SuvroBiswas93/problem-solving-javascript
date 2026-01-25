8e5cfbfa09551ba85afe43c7956510cbfd4e736a
Suvro Biswas <shuvo.249963@gmail.com>
2026-01-25 20:46:20 +0600

    solve flattenArray problem

---
+++ flattenArray.js
@@ -0,0 +1,17 @@
+function flattenArray(arr) {
+  let result = [];
+
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      result = result.concat(flattenArray(item));
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
+
+
+const finalResult = flattenArray([1,2,[3,4],5]);
+console.log(finalResult)
